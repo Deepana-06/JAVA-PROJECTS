423b618d8b2da1121cf62a4eb8ce8f8c8aef3b09
Deepana-06 <144882174+Deepana-06@users.noreply.github.com>
2025-11-13 18:15:31 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,33 @@
+// File Name: BankAccount.java
+public class BankAccount {
+    private String accountHolder;
+    private int accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolder, int accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        if(amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    public void display() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+        System.out.println("------------------------");
+    }
+}
