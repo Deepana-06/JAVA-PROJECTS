a5b7d49358e4239a3ef3dc36db5e60515ac2fd12
Deepana-06 <144882174+Deepana-06@users.noreply.github.com>
2025-11-13 18:13:08 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,34 @@
+// File Name: Student.java
+public class Student {
+    private String name;
+    private int age;
+    private String grade;
+
+    // Constructor
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    // Display student details
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("------------------------");
+    }
+}
